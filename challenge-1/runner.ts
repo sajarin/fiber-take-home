f3cd43dddd518690a3adbbfef412f0775c4d3426
sajarindider <sajarindider@gmail.com>
2024-09-18 17:21:13 -0400

    feat: add extract.ts

--- challenge-1/runner.ts
+++ challenge-1/runner.ts
@@ -1,8 +1,20 @@
-import { processDataDump } from "./challenge";
+import { DUMP_DOWNLOAD_URL } from './resources';
+import { downloadFile } from './download';
+import { decompressAndExtract } from './extract';
 
-/**
- * This is the entry point for the challenge.
- * This will run your code.
- */
-await processDataDump();
-console.log("✅ Done!");
+const downloadPath: string = 'tmp/dump.tar.gz';
+const extractPath: string = 'tmp/';
+
+const main = async () => {
+  try {
+    // await downloadFile(DUMP_DOWNLOAD_URL, downloadPath);
+    // console.log('File downloaded successfully.');
+
+    await decompressAndExtract(downloadPath, extractPath);
+    console.log('File decompressed and extracted successfully.');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
+
+main();
