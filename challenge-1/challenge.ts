47128e36cb6f4c4a42a14b97982cdb2637bdf614
sajarindider <sajarindider@gmail.com>
2024-09-18 17:22:06 -0400

    fix: add outline of execution to challenge.ts

--- challenge-1/challenge.ts
+++ challenge-1/challenge.ts
@@ -1,4 +1,8 @@
-import * as fs from 'fs';
+import { downloadFile} from './download';
+import { decompressAndExtract } from './extract';
+import { setupDatabase } from './db';
+import { processCSV } from './csvProcessor';
+import { DUMP_DOWNLOAD_URL, SQLITE_DB_PATH, DUMP_LOCAL_PATH, EXTRACT_PATH} from './resources';
 
 /**
  * The entry point function. This will download the given dump file, extract/decompress it,
@@ -6,10 +10,21 @@ import * as fs from 'fs';
  * This is the core function you'll need to edit, though you're encouraged to make helper
  * functions!
  */
-export async function processDataDump() {
-  // download data
-  // decompress data
-  // parse csv
-  // add data to database
-}
+const processDataDump = async (): Promise<void> => {
+  console.log("processing data dump...")
+  const tarFileUrl = DUMP_DOWNLOAD_URL;
+  const downloadPath = DUMP_LOCAL_PATH;
+  const extractPath = EXTRACT_PATH;
+  const dbPath = SQLITE_DB_PATH
 
+  await downloadFile(tarFileUrl, downloadPath);
+  await decompressAndExtract(downloadPath, extractPath);
+
+  const db = await setupDatabase(dbPath);
+
+  await processCSV(`${extractPath}/customers.csv`, db, 'customers');
+  await processCSV(`${extractPath}/organizations.csv`, db, 'organizations');
+  console.log("✅ Done!");
+};
+
+export default processDataDump;
