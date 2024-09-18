b7d9e215c4b009a9414f2363b7c40ce8453c65b6
sajarindider <sajarindider@gmail.com>
2024-09-18 13:31:44 -0400

    init: start challenge

---
+++ challenge-1/challenge.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+
+/**
+ * The entry point function. This will download the given dump file, extract/decompress it,
+ * parse the CSVs within, and add the data to a SQLite database.
+ * This is the core function you'll need to edit, though you're encouraged to make helper
+ * functions!
+ */
+export async function processDataDump() {
+  // download data
+  // decompress data
+  // parse csv
+  // add data to database
+}
+
