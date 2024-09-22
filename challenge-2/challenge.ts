cdd8bb9270e290f785ff9b923babf661d3a0fde7
sajarindider <sajarindider@gmail.com>
2024-09-22 17:30:27 -0400

    chore: add some testing inputs to use

--- challenge-2/challenge.ts
+++ challenge-2/challenge.ts
@@ -1,9 +1,23 @@
+import * as fs from "fs-extra";
+import { parseCSV } from "./src/parser";
+import { scrapeCompany } from "./src/scraper";
+import { JSON_OUTPUT_PATH } from "./resources";
+import chalk from "chalk";
+
 /**
  * The entry point function. This will read the provided CSV file, scrape the companies'
  * YC pages, and output structured data in a JSON file.
  */
 export async function processCompanyList() {
-  /**
-   * Put your code here!
-   */
+  const companyList = await parseCSV();
+
+  // For testing purposes, only scrape the first company and output its contents
+  const result = await scrapeCompany(companyList[0]);
+
+  console.log(chalk.rgb(222, 173, 237)("Scraped data for the first company:"));
+  console.log(result);
+  // await fs.outputJSON(JSON_OUTPUT_PATH, result);
+
+  // const result = await Promise.all(companyList.map(scrapeCompany));
+  // await fs.outputJson(JSON_OUTPUT_PATH, result, { spaces: 2 });
 }
--- challenge-2/resources.ts
+++ challenge-2/resources.ts
@@ -2,6 +2,7 @@
  * Read this CSV file to
  */
 export const CSV_INPUT_PATH: string = "inputs/companies.csv";
+export const TEST_CSV_INPUT_PATH: string = "inputs/test.csv";
 
 /**
  * Your JSON file, containing scraped and structured data from all the companies in
